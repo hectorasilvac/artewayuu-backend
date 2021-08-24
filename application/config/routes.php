bed07f34274e4f8928333f3f57ffa6be65261a01
silvahector <hascardenas@gmail.com>
2021-08-24 03:17:42 -0500

    Change routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,9 +49,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['users'] = 'users/view';
+$route['categories/(:any)'] = 'categories/$1';
+
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-$route['users'] = 'users/view';
-$route['categories/(:any)'] = 'categories/$1';
