4eeb50d5dea85c50d6ab38c938e29f4baa641492
silvahector <hascardenas@gmail.com>
2021-08-25 22:42:52 -0500

    xass

--- application/controllers/Discounts.php
+++ application/controllers/Discounts.php
@@ -11,7 +11,7 @@ class Discounts extends CI_Controller
 
 	public function add()
 	{
-		echo $this->input->input_stream('minimum');
+		var_dump($this->input->post());
 		exit();
 		// $result = [];
 
